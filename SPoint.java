beae02018c142f92cc9ed83f25bba8c111c63036
RahulB <rahulpikslit@gmail.com>
2020-01-21 20:17:26 +0530

    Documentation added

--- SPoint.java
+++ SPoint.java
@@ -1,24 +1,14 @@
+/**
+ * A class to define points with screen (integral) cordinates.
+ */
+
 public class SPoint
 {
-	public int x, y; 
+	public int x, y;
 	public SPoint(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
 	}
 
-	@Override
-	public int hashCode()
-	{
-		return y;
-	}
-
-	@Override
-	public boolean equals(Object p)
-	{
-		if(p instanceof SPoint)
-			return (((SPoint)p).x == x && ((SPoint)p).y == y);
-		System.out.print("Hurrah!!!");
-		return false;
-	}
 }
