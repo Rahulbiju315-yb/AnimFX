1a40d06eb6a6ffaa8bd4da8702f16d2a97f6c3c6
RahulB <rahulpikslit@gmail.com>
2020-01-21 20:05:02 +0530

    Documentation and minor edits

--- LineFX.java
+++ LineFX.java
@@ -4,31 +4,70 @@ import java.awt.event.*;
 import java.awt.*;
 import java.awt.image.*;
 
-
+/**
+ * A class implementing {@code Drawable} to draw basic lines and allow their animation.
+ * on a {@code GraphFX}
+ *
+ * <p>
+ * A {@code LineFX} can be created by either specifying its end points in real cordinates
+ * or in screen cordinates and also passing the {@code GraphFX} to which the drawing needs
+ * to be done. Note that this does not mean that the LineFX will be rendered on the screen.
+ * To do this the {@code addDrawable} method of the {@code GraphFX} must be called.
+ *
+ * <p>
+ * Though the cordinates are stored as scren - cordinates in {@code LineFX}, they are of type
+ * double. This is to ensure a continuous change in position on each update.
+ */
 public class LineFX extends Drawable
 {
-	BufferedImage bim;
+	/**
+	 * The end points' cordinates of the {@code LineFX} to be drawn.
+	 */
 	double x1, y1, x2, y2;
 
 	Graphics2D g2d;
-	boolean replot = false;
 
+	/**
+	 * Cordinates of the centre of the line. These are defined for calculting endpoints
+	 * following rotation updates.
+	 *
+	 * @see #update
+	 */
 	double cx, cy;
 
+	/**
+	 * The angle of inclination of the line.
+	 */
 	double theta;
+
+	/**
+	 * The length of the line.
+	 */
 	double l;
 
-	String ID;
-	String name;
-
+	/**
+	 * A static variable which counts the number of {@code LineFX} objects that
+	 * have been created, to give a unique name to each of the {@code LineFX}
+	 * objects.
+	 *
+	 * @see #LineFX
+	 */
 	static int count;
-	boolean withinBounds = true;
 
+	/**
+	 * Creates a {@code LineFX} with the given screen cordinates and associated with
+	 * the given {@code GraphFX}.
+	 *
+	 * @param x1 the screen x cordinate of the first endpoint.
+	 * @param y1 the screen y cordinate of the first endpoint.
+	 * @param x2 the screen x cordinate of the second endpoint.
+	 * @param y2 the screen y cordinate of the second endpoint.
+	 * @param gfx the {@code GraphFX} instance.
+	 */
 	public LineFX(int x1, int y1, int x2, int y2, GraphFX gfx)
 	{
-		this.bim = bim;
 		this.gfx = gfx;
-		htb = new Hashtable<SPoint, Color>();
+
 		updateEvents = new Vector<UpdateEvent>();
 
 		this.x1 = x1;
@@ -40,235 +79,81 @@ public class LineFX extends Drawable
 		cx = (x1 + x2)/2;
 		cy = (y1 + y2)/2;
 
-		if(bim != null)
-			g2d = (Graphics2D)bim.getGraphics();
 		l = Math.pow(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2), 1/2.0);
-
 		theta = (x2 == x1) ? Math.PI / 2 : Math.atan((y2 - y1) / (x2 - x1));
 
 		count++;
-		ID = "LineFX_" + count;
-		name = new String(ID);
+		name = "LineFX_" + count;
 	}
 
+	/**
+	 * Creates a {@code LineFX} with the given real cordinates.
+	 *
+	 * @param x1 the real x cordinate of the first endpoint.
+	 * @param y1 the real y cordinate of the first endpoint.
+	 * @param x2 the real x cordinate of the second endpoint.
+	 * @param y2 the real y cordinate of the second endpoint.
+	 */
 	public LineFX(double x1, double y1, double x2, double y2, GraphFX gfx)
 	{
 		this(gfx.rtsX(x1), gfx.rtsY(y1), gfx.rtsX(x2), gfx.rtsY(y2), gfx);
 	}
 
-	public void setID(String ID)
-    {
-        this.ID = new String(ID);
-    }
-
-    public void setName(String name)
-    {
-    	this.name = name;
-    }
-
-	public void setBImage(BufferedImage bim)
-	{
-		this.bim = bim;
-	}
-
+	/**
+	 * Shifts the line in the x direction by the specified real cordinates.
+	 *
+	 * @param x the amount to shift in x direction.
+	 */
 	@Override
 	public void xShift(double x)
 	{
-		x1 += x;
-		x2 += x;
+		double dx = gfx.rtsXD(x);
+		x1 += dx;
+		x2 += dx;
+		cx += dx;
 	}
 
-	@Override 
+	/**
+	 * Shifts the line in the x direction by the specified real cordinates.
+	 *
+	 * @param y the amount to shift in y direction.
+	 */
+	@Override
 	public void yShift(double y)
 	{
-		y1 += y;
-		y2 += y;
+		double dy = gfx.rtsYD(y);
+		y1 += dy;
+		y2 += dy;
+		cy += dy;
 	}
 
+	/**
+	 * Draws a line between ({@code x1}, {@code y1}) and ({@code x2}, {@code y2}).
+	 */
 	public void plot()
 	{
-		boolean fl = true;
-
-		if(bim == null)
-			return;
-		g2d = (Graphics2D)bim.getGraphics();
-		double dy = y2 - y1;
-		double dx = x2 - x1;
-		int sign = dy*dx > 0 ? 1 : (dx == 0 ? 0 : -1);
-
-		if(sign == -1 && Math.abs(dx) >= Math.abs(dy))
-		{
-			int x1, y1;
-			int x2, y2;
-			double err = 0;
-			double m = dy / dx;
-			if(this.x1 < this.x2)
-			{
-				x1 = (int)this.x1;
-				y1 = (int)this.y1;
-				x2 = (int)this.x2;
-				y2 = (int)this.y2;
-			}
-			else
-			{
-				x2 = (int)this.x1;
-				y2 = (int)this.y1;
-				x1 = (int)this.x2;
-				y1 = (int)this.y2;
-			}
-
-			for(int i = x1; i < x2; i++)
-			{
-				err += m;
-				if(err < -0.5)
-				{
-					err += 1;
-					y1 -= 1;
-				}
-				putPixel(i, y1, bim, replot);
-			}
-		}
-
-		if(sign == -1 && Math.abs(dx) < Math.abs(dy))
-		{
-			int x1, y1;
-			int x2, y2;
-			double err = 0;
-			double m = dx / dy;
-			if(this.y1 < this.y2)
-			{
-				x1 = (int)this.x1;
-				y1 = (int)this.y1;
-				x2 = (int)this.x2;
-				y2 = (int)this.y2;
-			}
-			else
-			{
-				x2 = (int)this.x1;
-				y2 = (int)this.y1;
-				x1 = (int)this.x2;
-				y1 = (int)this.y2;
-			}
-
-			for(int i = y1; i < y2; i++)
-			{
-				err += m;
-				if(err < -0.5)
-				{
-					err += 1;
-					x1 -= 1;
-				}
-				putPixel(x1, i, bim, replot);
-			}
-		}
-
-		if(sign == 1 && Math.abs(dx) >= Math.abs(dy))
-		{
-			int x1, y1;
-			int x2, y2;
-			double err = 0;
-			double m = dy / dx;
-			if(this.x1 < this.x2)
-			{
-				x1 = (int)this.x1;
-				y1 = (int)this.y1;
-				x2 = (int)this.x2;
-				y2 = (int)this.y2;
-			}
-			else
-			{
-				x2 = (int)this.x1;
-				y2 = (int)this.y1;
-				x1 = (int)this.x2;
-				y1 = (int)this.y2;
-			}
-
-			for(int i = x1; i < x2; i++)
-			{
-				err += m;
-				if(err > 0.5)
-				{
-					err -= 1;
-					y1 += 1;
-				}
-				putPixel(i, (int)y1, bim, replot);
-			}
-		}
-
-		if(sign == 1 && Math.abs(dx) < Math.abs(dy))
-		{
-			int x1, y1;
-			int x2, y2;
-			double err = 0;
-			double m = dx / dy;
-			if(this.y1 < this.y2)
-			{
-				x1 = (int)this.x1;
-				y1 = (int)this.y1;
-				x2 = (int)this.x2;
-				y2 = (int)this.y2;
-			}
-			else
-			{
-				x2 = (int)this.x1;
-				y2 = (int)this.y1;
-				x1 = (int)this.x2;
-				y1 = (int)this.y2;
-			}
-
-			for(int i = y1; i < y2; i++)
-			{
-				err += m;
-				if(err > 0.5)
-				{
-					err -= 1;
-					x1 += 1;
-				}
-				putPixel((int)x1, i, bim, replot);
-			}
-		}
-
-		if(sign == 0)
-		{
-			int y1;
-			int y2;
-			if(this.y1 < this.y2)
-			{
-				y1 = (int)this.y1;
-				y2 = (int)this.y2;
-			}
-			else
-			{
-				y2 = (int)this.y1;
-				y1 = (int)this.y2;
-			}
-			for(int i = y1; i < y2; i++)
-			{
-				putPixel((int)this.x1, i, bim, replot);
-			}
-		}
+		((Graphics2D)bim.getGraphics()).drawLine((int)(x1 + 0.5), (int)(y1 + 0.5), (int)(x2 + 0.5), (int)(y2 + 0.5));
 	}
 
+	/**
+	 * Sets the color of the line.
+	 *
+	 * @param c the color of the line.
+	 */
 	public void setColor(Color c)
 	{
-		g2d.setColor(c);
+		((Graphics2D)bim.getGraphics()).setColor(c);
 	}
 
-	public void unplot()
-	{
-		replot = true;
-		plot();
-		replot = false;
-	}
-
-	public void changeCords(int x1, int y1, int x2, int y2)
-	{
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-	}
-
+	/**
+	 * The update function.
+	 *
+	 * <p>
+	 * The values of endpoints are updated based on the velocity and error factor. The error factor
+	 * {@code err} is the factor of the frame time by which the previous plot-update cycle finished
+	 * late by. For example is the frame time is 20 ms and it took the previous plot-update cycle
+	 * finished in 25 ms the error factor will be (25 - 20) /
+	 */
 	public void update(double err)
 	{
 		super.update(err);
@@ -293,35 +178,37 @@ public class LineFX extends Drawable
 			x2 = cx - (l / 2 * Math.cos(phi_ + theta));
 			y2 = cy - (l / 2 * Math.sin(phi_ + theta));
 
-			//putPixel(x1, y1, bim, false);
 			theta += phi_;
 		}
-		//omega += omega;
 	}
 
+
 	public String toString()
-    {
-        return name;
-    }
+   {
+   	return name;
+   }
 
-    public String getID()
-    {
-    	return ID;
-    }
+	/**
+	 * Returns the bounding rectangle of the {@code LineFX}.
+	 *
+	 * @see Drawable#getBounds
+	 */
+	@Override
+	public RealRectangle getBounds()
+	{
+   	return getBoundedRectangle(x1, y1, x2, y2);
+   }
 
-    @Override
-    public Rectangle getBounds()
-    {
-    	return getBoundedRectangle((int)x1, (int)y1, (int)x2, (int)y2);
-    }
-
-    @Override 
-    public Object clone()
-    {
-    	LineFX lineClone = new LineFX((int)x1, (int)y1, (int)x2, (int)y2, gfx);
+	/**
+	 * Returns a clone of the current {@code LineFX} instance.
+	 */
+	@Override
+	public Object clone()
+   {
+   	LineFX lineClone = new LineFX((int)x1, (int)y1, (int)x2, (int)y2, gfx);
     	lineClone.setLinearVel(vx, vy);
     	lineClone.setAngularVel(phi);
-    	
+
     	if(updateEvents != null)
 			for(int i = 0; i < ueSize; i++)
 			{
@@ -333,5 +220,3 @@ public class LineFX extends Drawable
 		return lineClone;
     }
 }
-
-
