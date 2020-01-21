8152283144f279de911b09ed2afdb71139c0deff
RahulB <rahulpikslit@gmail.com>
2020-01-21 23:52:22 +0530

    Added documentation. Added missing error parameter to the update function.

--- UpdateEvent.java
+++ UpdateEvent.java
@@ -5,8 +5,22 @@ import java.awt.*;
 import java.awt.image.*;
 
 
-
+/**
+ * Definition for an update event.
+ *
+ * <p>
+ * An update event performs a set of parameter updates on the given
+ * {@code Drawable}
+ */
 public abstract class UpdateEvent
 {
-	public abstract void update(Drawable d);
+	/**
+	 * The method which is called by the update method of the Drawable
+	 * class.
+	 *
+	 * @param d the {@code Drawable} which is calling the update function.
+	 * @param err the error parameter with which the update method of
+	          the {@code Drawable} had been called.
+	 */
+	public abstract void update(Drawable d, double err);
 }
